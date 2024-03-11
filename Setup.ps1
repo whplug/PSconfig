adaad28ce81b39ebe93cc553cf70d3336e0cc54a
whplug <whplug@gmail.com>
2024-03-11 17:30:51 +0100

    .

---
+++ Setup.ps1
@@ -0,0 +1,13 @@
+Set-Alias k kubectl
+
+function Invoke-KubectlRun {
+    param(
+        [string]$PodName,
+        [string]$Image
+    )
+
+    $command = "kubectl run $PodName --image=$Image --dry-run=client -o yaml"
+    Invoke-Expression $command
+}
+
+Set-Alias -Name krun -Value Invoke-KubectlRun
